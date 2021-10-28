e27695d4813763757694801576f169a059685516
gabriel.camurca <gabriel.camurca@insightlab.ufc.br>
2021-10-27 21:50:12 -0300

    Versal Inicial da Esteganografia (Não Completa)

--- img.py
+++ img.py
@@ -1,8 +1,11 @@
 from PIL import Image
 import os
+import io
 import numpy as np
 import matplotlib.pyplot as plt
 import functions as fc
+import cv2
+from math import ceil
 
 
 class Img():
@@ -177,3 +180,89 @@ class Img():
         temp_img = (temp_img * 255).astype(np.uint8)
         self.img_now = temp_img
         return Image.fromarray(self.img_now)
+    
+    def img_to_array(self):
+        img_byte_arr = io.BytesIO()
+        self.img.save(img_byte_arr, format=self.img.format)
+        img_byte_arr = img_byte_arr.getvalue()
+        return img_byte_arr
+
+    def encrypt(data, imagem):
+        # img = Image.convert(imagem, 'RGB')
+        # img = np.array(img)
+        img = cv2.cvtColor(np.asarray(imagem), cv2.COLOR_BGR2RGB)
+        # imagem = Image(imagem).convert('RGB')
+        # img_t = np.array(imagem)
+        # img = img_t[:, :, ::-1].copy()
+
+        # break the image into its character level. Represent the characyers in ASCII.
+        data = [format(ord(i), '08b') for i in data]
+        _, width, _ = img.shape
+        # algorithm to encode the image
+        PixReq = len(data) * 3
+
+        RowReq = PixReq/width
+        RowReq = ceil(RowReq)
+
+        count = 0
+        charCount = 0
+        # Step 3
+        for i in range(RowReq + 1):
+            # Step 4
+            while(count < width and charCount < len(data)):
+                char = data[charCount]
+                charCount += 1
+                # Step 5
+                for index_k, k in enumerate(char):
+                    if((k == '1' and img[i][count][index_k % 3] % 2 == 0) or (k == '0' and img[i][count][index_k % 3] % 2 == 1)):
+                        img[i][count][index_k % 3] -= 1
+                    if(index_k % 3 == 2):
+                        count += 1
+                    if(index_k == 7):
+                        if(charCount*3 < PixReq and img[i][count][2] % 2 == 1):
+                            img[i][count][2] -= 1
+                        if(charCount*3 >= PixReq and img[i][count][2] % 2 == 0):
+                            img[i][count][2] -= 1
+                        count += 1
+            count = 0
+        # Step 6
+        # Write the encrypted image into a new file
+        cv2.imshow("encrypted image", img)
+        # cv2.imwrite("encrypted_image.png", img)
+        return img
+    
+    def decrypt(img):
+
+        data = []
+        stop = False
+        for index_i, i in enumerate(img):
+            i.tolist()
+            for index_j, j in enumerate(i):
+                if((index_j) % 3 == 2):
+                    # first pixel
+                    data.append(bin(j[0])[-1])
+                    # second pixel
+                    data.append(bin(j[1])[-1])
+                    # third pixel
+                    if(bin(j[2])[-1] == '1'):
+                        stop = True
+                        break
+                else:
+                    # first pixel
+                    data.append(bin(j[0])[-1])
+                    # second pixel
+                    data.append(bin(j[1])[-1])
+                    # third pixel
+                    data.append(bin(j[2])[-1])
+            if(stop):
+                break
+
+        message = []
+        # join all the bits to form letters (ASCII Representation)
+        for i in range(int((len(data)+1)/8)):
+            message.append(data[i*8:(i*8+8)])
+        # join all the letters to form the message.
+        message = [chr(int(''.join(i), 2)) for i in message]
+        message = ''.join(message)
+
+        print(message)
