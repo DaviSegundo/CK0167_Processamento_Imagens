ae34c454e5fafdb3fc462924155e051fec8c3ae5
Davi S.P <davisp2009@hotmail.com>
2021-12-07 22:35:34 -0300

    New Implementations - Filters & Others

--- functions.py
+++ functions.py
@@ -2,6 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
+def create_circular_mask(h, w, center=None, radius=None):
+
+    if center is None: # use the middle of the image
+        center = (int(w/2), int(h/2))
+    if radius is None: # use the smallest distance between the center and image walls
+        radius = min(center[0], center[1], w-center[0], h-center[1])
+
+    Y, X = np.ogrid[:h, :w]
+    dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
+
+    mask = np.uint8(dist_from_center <= radius)
+    return mask
+
+def from_array(image_data):
+  return Image.fromarray(np.uint8(np.clip(image_data * 255, 0, 255)))
+
 def transform_points(text):
     list_s = text.split(",")
 
